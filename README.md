d58d861e4e0072b419e67b29b35da56996269295
Joshua <jopenrod91@gmail.com>
2024-01-30 20:12:35 -0700

    working on auth more. fingers crossed to implement the admin page

--- README.md
+++ README.md
@@ -2,3 +2,23 @@
 This is a full stack CRUD web application utilizing Javascript/React/Express/Knex/PostgresSQL
 
 start off my navigating into frontend and running 'npm i'
+
+navigate to the backend and run 'npm start'
+navigate to the frontend and run 'npm start'
+
+Admin accounts are
+1.
+  Username: Admin@admin.com
+  Password: admin
+2.
+  Username: manage@admin.com
+  Password: arnie_palmie
+
+The theme of my application was an online crystal retailer.
+The home page gives a brief description of the different types of crystals in inventory, and a button will direct you to each of the different varieties.
+
+The header consists of several buttons.
+the first is the image of a green pixel crystal. that will navigate you back to the home page.
+The cog wheel is supposed to take you to the admin page, if you logged in and have the appropriate cookie. From here you would be able to see the full inventory, add new items, and delete items.
+The shopping cart, was a stretch goal to implement, but clicking it adds to a counter that increases until 99.
+The fingerprint icon/button is how you log in.
--- frontend/src/pages/Admin.js
+++ frontend/src/pages/Admin.js
@@ -1,20 +1,21 @@
-import React from 'react';
 import { Grid, Paper } from '@mui/material';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import cookie from 'cookie';
 
 const Admin = () => {
   const [crystals, setCrystals] = useState([]);
+  const [hasAccess, setHasAccess] = useState(false);
   const cookies = cookie.parse(document.cookie);
-
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         if (!cookies.Admin) {
-          return <div color="white">You do not have access. Please return home and login.</div>;
+          setHasAccess(false);
+          return;
         }
 
+        setHasAccess(true)
         const response = await fetch('http://localhost:8080/crystal');
         if (!response.ok) {
           throw new Error('Failed to fetch crystals');
@@ -33,23 +34,27 @@ const Admin = () => {
 
   return (
     <>
-      {crystals.map((crystal) => (
-        <Grid container spacing={2} key={crystal.id}>
-          <Grid item xs={4}>
-            <Paper sx={{ backgroundColor: '#BCD5CF' }}>
-              <div>
-                <strong>Name:</strong> {crystal.Name}
-              </div>
-              <div>
-                <img src={crystal.Image} alt={crystal.Name} />
-              </div>
-              <div>
-                <strong>Quantity:</strong> {crystal.Quantity}
-              </div>
-            </Paper>
+      {hasAccess ? (
+        crystals.map((crystal) => (
+          <Grid container spacing={2} key={crystal.id}>
+            <Grid item xs={4}>
+              <Paper sx={{ backgroundColor: '#BCD5CF' }}>
+                <div>
+                  <strong>Name:</strong> {crystal.Name}
+                </div>
+                <div>
+                  <img src={crystal.Image} alt={crystal.Name} />
+                </div>
+                <div>
+                  <strong>Quantity:</strong> {crystal.Quantity}
+                </div>
+              </Paper>
+            </Grid>
           </Grid>
-        </Grid>
-      ))}
+        ))
+      ) : (
+        <div style={{color:'white'}}>You do not have access. Please return home and login.</div>
+      )}
     </>
   );
 };
